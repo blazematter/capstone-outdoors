f1e093f6009e82a111b454d951fae80d74e83f88
Andy Ho <andyho9211224@gmail.com>
2023-05-26 08:55:50 -0400

    done, but maybe add more css before presentation

--- information-page/main.js
+++ information-page/main.js
@@ -1,21 +1,23 @@
-const mountainSelect = document.getElementById('mountainSelect')
-const mountainInfo = document.getElementById('mountainInfo')
+const mountainSelect = document.getElementById("mountainSelect");
+const mountainInfo = document.getElementById("mountainInfo");
 
 mountainsArray.forEach((mountain, index) => {
-    let option = document.createElement('option')
-    option.value = index
-    option.textContent = mountain.name
-    mountainSelect.appendChild(option)
+  let option = document.createElement("option");
+  option.value = index;
+  option.textContent = mountain.name;
+  mountainSelect.appendChild(option);
 });
 
-
-mountainSelect.addEventListener('change', function() {
-    
-    let selectedMountain = mountainsArray[this.value]
-    document.getElementById('mountainName').textContent = selectedMountain.name
-    document.getElementById('mountainImage').src = "images/" + selectedMountain.img
-    document.getElementById('mountainDesc').textContent = selectedMountain.desc
-    document.getElementById('mountainElevation').textContent = selectedMountain.elevation
-    document.getElementById('mountainEffort').textContent = selectedMountain.effort
-    document.getElementById('mountainCoords').textContent = selectedMountain.coords
+mountainSelect.addEventListener("change", function () {
+  let selectedMountain = mountainsArray[this.value];
+  const formattedCoords = `Latitude: ${selectedMountain.coords.lat}, Longitude: ${selectedMountain.coords.lng}`;
+  document.getElementById("mountainName").textContent = selectedMountain.name;
+  document.getElementById("mountainImage").src =
+    "images/" + selectedMountain.img;
+  document.getElementById("mountainDesc").textContent = selectedMountain.desc;
+  document.getElementById("mountainElevation").textContent =
+    selectedMountain.elevation;
+  document.getElementById("mountainEffort").textContent =
+    selectedMountain.effort;
+  document.getElementById("mountainCoords").textContent = formattedCoords;
 });
--- search-page/main.js
+++ search-page/main.js
@@ -1,62 +1,64 @@
+// i asked my brother for some help with direction for this code and also i got aid from this https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
+// loking at this again I thought i did not do this right, because I thought the second search was suppose to filter the first search even more, which explains why I stuggled so much
 
-
-// i asked my brother for a little help with direction for this code and also i got aid from this https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
-
-const stateSelect = document.getElementById("state-select")
-const parkTypeSelect = document.getElementById("park-type-select")
-const parkInfoContainer = document.getElementById("park-info")
-
+const stateSelect = document.getElementById("state-select");
+const parkTypeSelect = document.getElementById("park-type-select");
+const parkInfoContainer = document.getElementById("park-info");
 
 for (const state of locationsArray) {
-    const option = document.createElement("option")
-    option.value = state
-    option.textContent = state
-    stateSelect.appendChild(option)
+  const option = document.createElement("option");
+  option.value = state;
+  option.textContent = state;
+  stateSelect.appendChild(option);
 }
 
-
 stateSelect.addEventListener("change", () => {
-    const selectedState = stateSelect.value.toLowerCase()
-    const selectedParkType = parkTypeSelect.value.toLowerCase()
-    const filteredParks = filterParks(selectedState, selectedParkType)
-    displayParks(filteredParks)
+  const selectedState = stateSelect.value.toLowerCase();
+  const selectedParkType = parkTypeSelect.value.toLowerCase();
+  const filteredParks = filterParks(selectedState, selectedParkType);
+  displayParks(filteredParks);
 });
 
 for (const parkType of parkTypesArray) {
-    const option = document.createElement("option")
-    option.value = parkType
-    option.textContent = parkType
-    parkTypeSelect.appendChild(option)
+  const option = document.createElement("option");
+  option.value = parkType;
+  option.textContent = parkType;
+  parkTypeSelect.appendChild(option);
 }
-
 
 parkTypeSelect.addEventListener("change", () => {
-    const selectedState = stateSelect.value.toLowerCase()
-    const selectedParkType = parkTypeSelect.value.toLowerCase()
-    const filteredParks = filterParks(selectedState, selectedParkType)
-    displayParks(filteredParks)
+  const selectedState = stateSelect.value.toLowerCase();
+  const selectedParkType = parkTypeSelect.value.toLowerCase();
+  const filteredParks = filterParks(selectedState, selectedParkType);
+  displayParks(filteredParks);
 });
 
-
 function filterParks(selectedState, selectedParkType) {
-    if (selectedState === "all" && selectedParkType === "all") {
-        return nationalParksArray
-    } else if (selectedState === "all") {
-        return nationalParksArray.filter(park => park.LocationName.toLowerCase().includes(selectedParkType))
-    } else if (selectedParkType === "all") {
-        return nationalParksArray.filter(park => park.State.toLowerCase() === selectedState)
-    } else {
-        return nationalParksArray.filter(park => park.State.toLowerCase() === selectedState && park.LocationName.toLowerCase().includes(selectedParkType))
-    }
+  if (selectedState === "all" && selectedParkType === "all") {
+    return nationalParksArray;
+  } else if (selectedState === "all") {
+    return nationalParksArray.filter((park) =>
+      park.LocationName.toLowerCase().includes(selectedParkType)
+    );
+  } else if (selectedParkType === "all") {
+    return nationalParksArray.filter(
+      (park) => park.State.toLowerCase() === selectedState
+    );
+  } else {
+    return nationalParksArray.filter(
+      (park) =>
+        park.State.toLowerCase() === selectedState &&
+        park.LocationName.toLowerCase().includes(selectedParkType)
+    );
+  }
 }
 
-
 function displayParks(parks) {
-    parkInfoContainer.innerHTML = ""
+  parkInfoContainer.innerHTML = "";
 
-    for (const park of parks) {
-        const parkDiv = document.createElement("div")
-        parkDiv.innerHTML = `
+  for (const park of parks) {
+    const parkDiv = document.createElement("div");
+    parkDiv.innerHTML = `
             <h3>${park.LocationName}</h3>
             <p>Address: ${park.Address}</p>
             <p>City: ${park.City}</p>
@@ -67,10 +69,9 @@ function displayParks(parks) {
             <p>Latitude: ${park.Latitude}</p>
             <p>Longitude: ${park.Longitude}</p>
             <a href="${park.Visit}" target="_blank">Visit Website</a>
-        `
-        parkInfoContainer.appendChild(parkDiv)
-    }
+        `;
+    parkInfoContainer.appendChild(parkDiv);
+  }
 }
 
-
-displayParks(nationalParksArray)
+displayParks(nationalParksArray);
